597c1a88cc89f4dd070bb7e8940b0775a9905d9e
Tomas Bambas <xbamba01@stud.fit.vutbr.cz>
2014-03-04 11:45:56 +0100

    Maven pwm project founded

---
+++ pwm/src/main/java/jenkins/python/pwm/WrapperMaker.java
@@ -0,0 +1,13 @@
+package jenkins.python.pwm;
+
+/**
+ * Hello world!
+ *
+ */
+public class WrapperMaker 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
