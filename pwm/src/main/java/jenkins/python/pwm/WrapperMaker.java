9b6bcd3e92162cb8110b64addbe3ed57c1700cb5
Tomas Bambas <xbamba01@stud.fit.vutbr.cz>
2014-03-04 17:45:11 +0100

    main class updated

--- pwm/src/main/java/jenkins/python/pwm/WrapperMaker.java
+++ pwm/src/main/java/jenkins/python/pwm/WrapperMaker.java
@@ -1,13 +1,52 @@
 package jenkins.python.pwm;
 
+import java.io.File;
+import java.util.List;
+import java.util.LinkedList;
+
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+
 /**
- * Hello world!
- *
+ * Main class of the pwm tool.
  */
-public class WrapperMaker 
+public class WrapperMaker
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String args[]){
+		File inputDir = getInputDir(args);
+        File outputDir = getOutputDir(args);
+        List<List<TypeDeclaration>> expoints = findAllExpoints(inputDir);
+        List<List<TypeDeclaration>> descriptors = findAllDescriptors(inputDir);
+        makeExpointWrappers(expoints, outputDir);
+        makeDescrWrappers(descriptors, outputDir);
+	}
+    
+    /**
+     * Checks and returns a path to the Jenkins source code directory.
+     */
+    private static File getInputDir(String args[]) {
+        return new File("/");
+    }
+    
+    /**
+     * Checks and returns a path to the python-wrapper plugin source code directory.
+     */
+    private static File getOutputDir(String args[]) {
+        return new File("/");
+    }
+    
+    private static List<List<TypeDeclaration>> findAllExpoints(File srcDir) {
+        return new LinkedList<List<TypeDeclaration>>();
+    }
+    
+    private static List<List<TypeDeclaration>> findAllDescriptors(File srcDir) {
+        return new LinkedList<List<TypeDeclaration>>();
+    }
+    
+    private static void makeExpointWrappers(List<List<TypeDeclaration>> expoints, File outputDir) {
+        
+    }
+    
+    private static void makeDescrWrappers(List<List<TypeDeclaration>> descriptors, File outputDir) {
+        
     }
 }
